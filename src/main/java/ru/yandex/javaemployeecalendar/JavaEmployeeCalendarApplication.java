f160e1c3f88a45f025a4aec1aec9798d57edf4c8
ViktorB <vitekb650@gmail.com>
2024-01-15 14:43:03 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/javaemployeecalendar/JavaEmployeeCalendarApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.javaemployeecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmployeeCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEmployeeCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/javaemployeecalendar/JavaEmployeeCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.javaemployeecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmployeeCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
